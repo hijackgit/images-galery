5d545a73b34bb3061b0d92022c073ae75231093c
hijackgit <marioreis@gmail.com>
2023-03-14 13:53:11 -0300

    api call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Search from './components/Search';
 import { useState } from 'react';
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
   const [word, setWord] = useState('');
@@ -10,7 +11,17 @@ function App() {
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
+    fetch(`https://api.unsplash.com/photos/random/?client_id=${UNSPLASH_KEY}&query=${word}`)
+    .then((res)=> res.json())
+    .then((data) => {
+      console.log(data)
+    })
+    .catch((err) =>{
+    })
   }
+
+
+  console.log(UNSPLASH_KEY);
 
   return (
     <div className="App">
